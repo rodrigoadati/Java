22933c31f9b64a26830bf2444f8f21da4d0299f3
rodrigoadati <rodrigo_adati@hotmail.com>
2015-02-03 14:45:24 -0200

    Primeiro commit

---
+++ FormatadorRecibo.java
@@ -0,0 +1,14 @@
+public class FormatadorRecibo{
+	private String nomeEmpresa;
+
+	public void setNomeEmpresa(String nomeEmpresa){
+		this.nomeEmpresa = nomeEmpresa;
+	}
+	public String getNomeEmpresa(){
+		return this.nomeEmpresa;	
+	}
+
+	public String gerarRecibo(String nomeCliente, double valor, String data, String motivo){
+		return "método em construção...";
+	}
+}
